93a55533db9e6ec5c076da8b7f0dcc8e1091706f
Mohinish Joshi <mohinishjoshi@gmail.com>
2022-12-29 18:57:34 +0530

    Initial Commit with D3 and React integrated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import BarChartData from './components/BarChartData'
+
+function App() {
+  return (
+    <StrictMode>
+      <BarChartData />
+    </StrictMode>
+  )
+}
+
+export default App;
